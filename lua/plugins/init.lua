94134a0d60df749a22334b9755487867e218e54d
Yash <yashrtalele@gmail.com>
2024-05-18 12:24:36 +0530

    Added null-ls

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -95,29 +95,7 @@ return {
       }
     },
     config = function()
-      require("noice").setup {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-          signature = {
-            enabled = false
-          },
-          hover = {
-            enabled = false
-          },
-        },
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      }
+      require "configs.noice"
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -137,6 +115,13 @@ return {
   {
     "tpope/vim-fugitive",
     lazy = false,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    config = function()
+      require "configs.null-ls"
+    end,
   },
   -- These are some examples, uncomment them if you want to see them work!
   -- {
