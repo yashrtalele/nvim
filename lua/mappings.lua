495612ea2230613f2aa73ca702f804e2a00ab252
Yash <yashrtalele@gmail.com>
2024-05-18 21:23:40 +0530

    Added mappings for - quit all, viewing git branches, preview hunk and current line blame

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,11 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+-- quit-all
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- git
+map("n", "<leader>gb", "<cmd> Telescope git_branches <CR>", { desc = "Git branches" })
+map("n", "<leader>gp", "<cmd> Gitsigns preview_hunk <CR>", { desc = "Preview Hunk" })
+map("n", "<leader>gb", "<cmd> Gitsigns toggle_current_line_blame <CR>", { desc = "Toggle current line blame" })
